fee180173c51de57f290e90fdd5702e06e4874c8
MinhoiKoo <minlove2013@gmail.com>
2024-06-02 01:07:05 +0900

    feat: 마이페이지 구현

---
+++ app/src/main/java/com/neordi6th/ddoring/ui/mypage/MypageActivity.kt
@@ -0,0 +1,17 @@
+package com.neordi6th.ddoring.ui.mypage
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.neordi6th.ddoring.R
+
+class MypageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_mypage)
+
+    }
+}
