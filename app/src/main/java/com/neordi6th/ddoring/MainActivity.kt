c46e0fb0dbbc2624e20f7028ac937d5ff3c613e0
angkmfirefoxygal <angkmfirefoxygal@sookmyung.ac.kr>
2024-06-02 02:29:43 +0900

    20240601-12:50pm

--- app/src/main/java/com/neordi6th/ddoring/MainActivity.kt
+++ app/src/main/java/com/neordi6th/ddoring/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.neordi6th.ddoring
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        var mybtn : ImageButton = findViewById(R.id.next_btn_2)
     }
+
+
 }
