0fe8203d004c3ecfa9aee77a88fb7044dd5af49b
MinhoiKoo <minlove2013@gmail.com>
2024-06-02 03:13:36 +0900

    refactor: 바인딩 추가

--- app/src/main/java/com/neordi6th/ddoring/ui/mypage/EditDdoringActivity.kt
+++ app/src/main/java/com/neordi6th/ddoring/ui/mypage/EditDdoringActivity.kt
@@ -1,17 +1,15 @@
 package com.neordi6th.ddoring.ui.mypage
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
 import com.neordi6th.ddoring.R
+import com.neordi6th.ddoring.databinding.ActivityEditDdoringBinding
 
 class EditDdoringActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEditDdoringBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_edit_ddoring)
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_edit_ddoring)
     }
 }
