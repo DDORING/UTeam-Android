38c2977bd148cfd619a6dbd2b483cb4cf04d1ace
angkmfirefoxygal <angkmfirefoxygal@sookmyung.ac.kr>
2024-06-01 23:01:40 +0900

    20240601-11:01pm

--- app/src/main/java/com/neordi6th/ddoring/CharacterAdapter.kt
+++ app/src/main/java/com/neordi6th/ddoring/CharacterAdapter.kt
@@ -1,24 +1,31 @@
 package com.neordi6th.ddoring
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.neordi6th.ddoring.databinding.ItemRcviewLoginBinding
 
 class CharacterAdapter(private val dataSet:ArrayList<CharacterData>) : RecyclerView.Adapter<CharacterAdapter.ViewHolder>() {
 
 
-    class ViewHolder :RecyclerView.ViewHolder(){
-
+    class ViewHolder(private val binding: ItemRcviewLoginBinding) :RecyclerView.ViewHolder(binding.root){
+        fun bind(item: CharacterData){
+            binding.message.text = item.name
+            binding.message2.text = item.content
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemRcviewLoginBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return dataSet.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(dataSet[position])
     }
 }
